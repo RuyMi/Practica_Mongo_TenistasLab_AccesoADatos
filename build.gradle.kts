2cfe60b661b0717d72ab504cff61d39fd0419e09
Rubén García-Redondo Marín <rubengrm@icloud.com>
2023-01-10 19:36:39 +0100

    Preparación Proyecto

---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.21"
+    application
+    kotlin("plugin.serialization") version "1.7.21"
+
+    // Para ktorfit que usa KSP
+    // Plugin KSP para generar código en tiempo de compilación ktorfit
+    id("com.google.devtools.ksp") version "1.7.21-1.0.8"
+}
+
+group = "es.ra"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+    // Para serializar JSON
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+
+    // Para hacer el logging
+    implementation("io.github.microutils:kotlin-logging-jvm:3.0.4")
+    implementation("ch.qos.logback:logback-classic:1.4.5")
+
+    // Ktorfit, es decir Ktor client modificado para parecerse a Retrofit
+    ksp("de.jensklingenberg.ktorfit:ktorfit-ksp:1.0.0-beta16")
+    implementation("de.jensklingenberg.ktorfit:ktorfit-lib:1.0.0-beta16")
+
+    // Para serializar en Json con Ktor
+    implementation("io.ktor:ktor-client-serialization:2.1.3")
+    implementation("io.ktor:ktor-client-content-negotiation:2.1.3")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.1.3")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "Practica_Mongo_TenistasLab_AccesoADatos"
+
